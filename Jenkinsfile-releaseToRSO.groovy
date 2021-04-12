3bad1caa1ae5a31ea63b18cb1dca7a5c11f40a97
Alexandre Santos <asantos@sonatype.com>
2021-04-12 16:43:14 -0700

    pipeline commit
    Adds required files for pipeline

---
+++ Jenkinsfile-releaseToRSO.groovy
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2011-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/clm/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+@Library(['private-pipeline-library']) _
+
+/**
+ * GitHub Pull Request builder.
+ *
+ * The real Jenkinsfile logic is in our shared Jenkins library:
+ *   https://github.com/sonatype/pipeline-jenkins-common/
+ * That library must be added to the Jenkins job as a Pipeline Library for this to work.
+ *
+ * We're using a style from this example, to get code reuse across scheduled-services:
+ * https://jenkins.io/doc/book/pipeline/shared-libraries/#defining-a-more-structured-dsl
+ */
+stagingWorkflow {
+  gitHubRepoPath = 'sonatype/dropwizard-swagger'
+  channelToNotify = 'slack-tests'
+}
